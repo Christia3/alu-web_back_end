d8cf8255eab6304790e384c7fec2b98c86bbe773
Christian Ishimwe Ntwali <128160015+Christia3@users.noreply.github.com>
2026-06-20 15:47:25 +0200

    alu-backend

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Task 5"""
-from flask import Flask, g, render_template, request
+"""Basic Flask app that implements i18n and internationalization"""
+
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -13,45 +14,48 @@ users = {
 }
 
 
-class Config(object):
-    """simple configuration"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """Config class for your application, it deals with babel mostly"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-
-
-def get_user():
-    """get user from header"""
-    id = request.args.get('login_as')
-    try:
-        return users.get(int(id))
-    except Exception:
-        return None
-
-
-@app.before_request
-def before_request():
-    """Before request used to stash user"""
-    g.user = get_user()
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """locale selector determining lang use for template"""
-    loc= request.args.get('locale')
-    if loc and loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    """Get locale for your application"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/", methods=['GET'])
-def index():
-    """index rotue"""
-    return render_template("6-index.html")
+def get_user():
+    """Get user information from users dict"""
+    login_as = request.args.get('login_as')
+    if login_as:
+        try:
+            return users.get(int(login_as))
+        except (ValueError, TypeError):
+            return None
+    return None
+
+
+@app.before_request
+def before_request():
+    """Before request - sets g.user if user is logged in"""
+    g.user = get_user()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Home page for your application"""
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
