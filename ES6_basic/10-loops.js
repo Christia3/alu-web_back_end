822aad762dc50bd2c4013d0b0cb9562888ffd6bb
Christian Ishimwe Ntwali <128160015+Christia3@users.noreply.github.com>
2026-06-29 09:39:15 +0200

    alu backend

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-nd
+export default function appendToEachArrayValue(array, appendString) {
+  const res = [];
+
+  for (const value of array) {
+    res.push(appendString + value);
+  }
+
+  return res;
+}
